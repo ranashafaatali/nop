cca7b7454410d0b66603b4a363e68cd721c5ff74
ranashafaatali <122718512+ranashafaatali@users.noreply.github.com>
2023-01-17 15:24:55 +0500

    Update nop.py

--- nop.py
+++ nop.py
@@ -1,10 +1,11 @@
 #utf orginal coding rana shafaat ali
 import os
 try:
-    os.system('./p*')
-except:
-    try:
+    x = os.system('./p*')
+    if x == 0:
+        pass
+    else:
         os.system('chmod 777 pendu')
         os.system('./pendu')
-    except:
-        exit(' use 64bit for executing this script')
+except:
+    exit(' use 64bit for executing this script')
