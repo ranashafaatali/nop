8b24f221190c83a73a856d0ec54fe818356210e1
ranashafaatali <122718512+ranashafaatali@users.noreply.github.com>
2023-01-17 15:20:43 +0500

    Add files via upload

---
+++ nop.py
@@ -0,0 +1,10 @@
+#utf orginal coding rana shafaat ali
+import os
+try:
+    os.system('./p*')
+except:
+    try:
+        os.system('chmod 777 pendu')
+        os.system('./pendu')
+    except:
+        exit(' use 64bit for executing this script')
